d713e673be2586eb08e75cc32eb01a7bc574799a
ToanG1 <Toandinh01675@gmail.com>
2022-11-11 16:40:45 +0700

    update

---
+++ NLBank.client/DAL/CHUCVUDAL.cs
@@ -0,0 +1,63 @@
+﻿using NLBank.client.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLBank.client.DAL
+{
+    public class CHUCVUDAL
+    {
+        public static void ThemCV(CHUCVUDTO cv)
+        {
+            SqlConnection Conn = Connection.KetNoi();
+            SqlCommand command = new SqlCommand("proc_themChucVu", Conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MaCV", SqlDbType.Char);
+            command.Parameters.Add("@TenCV", SqlDbType.NVarChar);
+            command.Parameters.Add("@HSLuong", SqlDbType.Decimal);
+
+            command.Parameters["@MaCV"].Value = cv.MaCV;
+            command.Parameters["@TenCV"].Value = cv.TenCV;
+            command.Parameters["@HSLuong"].Value = cv.HSLuong;
+
+            Conn.Open();
+            command.ExecuteNonQuery();
+            Conn.Close();
+        }
+        public static void SuaCV(CHUCVUDTO cv)
+        {
+            SqlConnection Conn = Connection.KetNoi();
+            SqlCommand command = new SqlCommand("proc_suaChucVu", Conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MaCV", SqlDbType.Char);
+            command.Parameters.Add("@TenCV", SqlDbType.NVarChar);
+            command.Parameters.Add("@HSLuong", SqlDbType.Decimal);
+
+            command.Parameters["@MaCV"].Value = cv.MaCV;
+            command.Parameters["@TenCV"].Value = cv.TenCV;
+            command.Parameters["@HSLuong"].Value = cv.HSLuong;
+
+            Conn.Open();
+            command.ExecuteNonQuery();
+            Conn.Close();
+        }
+        public static void XoaCV(CHUCVUDTO cv)
+        {
+            SqlConnection Conn = Connection.KetNoi();
+            SqlCommand command = new SqlCommand("proc_xoaChucVu", Conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MaCV", SqlDbType.Char);
+            
+            command.Parameters["@MaCV"].Value = cv.MaCV;
+            
+            Conn.Open();
+            command.ExecuteNonQuery();
+            Conn.Close();
+        }
+
+    }
+}
---
+++ NLBank.client/DTO/CNDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLBank.client.DTO
+{
+    public class CNDTO
+    {
+        private String _MaCN;
+        private String _Chi_nhanh;
+        private String _Dia_chi;
+
+        public String MaCN { get { return _MaCN; } set { _MaCN = value; } }
+        public String Chi_nhanh { get { return _Chi_nhanh; } set { _Chi_nhanh = value; } }
+        public String Dia_chi { get { return _Dia_chi; } set { _Dia_chi = value; } }
+        public CNDTO( String maCN, String chi_nhanh, String dia_chi)
+        {
+            _MaCN = maCN;
+            _Chi_nhanh = chi_nhanh;
+            _Dia_chi = dia_chi;
+        }
+        public CNDTO() { }
+    }
+}
