5453d67d76d1678a9ec7ad6aeba1d992c1dccb58
TOAN <toandungpqkg@gmail.com>
2022-11-15 16:40:56 +0700

    optimize login and add CN&DN to KH

--- NLBank.client/DAL/CANHANDAL.cs
+++ NLBank.client/DAL/CANHANDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NLBank.client.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace NLBank.client.DAL
 {
     class CANHANDAL
     {
+        public static CANHANDTO GetCaNhan(int MaKH) {
+            DataTable data = Connection.Instance.ExcuteQuery($"sp_LayCaNhanHayDoanhNghiep {MaKH}, 0");
+            foreach (DataRow item in data.Rows) { 
+                return new CANHANDTO(item);
+            }
+            return null;
+        }
     }
 }
--- NLBank.client/DAL/DOANHNGHIEPDAL.cs
+++ NLBank.client/DAL/DOANHNGHIEPDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NLBank.client.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace NLBank.client.DAL
 {
     class DOANHNGHIEPDAL
     {
+        public static DOANHNGHIEPDTO GetDoanhNghiep(int MaKH)
+        {
+            DataTable data = Connection.Instance.ExcuteQuery($"sp_LayCaNhanHayDoanhNghiep {MaKH}, 1");
+            foreach (DataRow item in data.Rows)
+            {
+                return new DOANHNGHIEPDTO(item);
+            }
+            return null;
+        }
     }
 }
