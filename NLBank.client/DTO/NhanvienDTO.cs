8019b655904d3cca85ed781267c6d1015f52b018
TOAN <toandungpqkg@gmail.com>
2022-11-15 18:35:28 +0700

    thay ChucVUDTo va sua mot it ben nhanvien

--- NLBank.client/DTO/NhanvienDTO.cs
+++ NLBank.client/DTO/NhanvienDTO.cs
@@ -1,7 +1,5 @@
-﻿using NLBank.client.DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +15,6 @@ namespace NLBank.client.DTO
         public string Email { set; get; }
         public string Sdt { set; get; }
         public int MaCV { set; get; }
-        private ChucVuDTO ChucVu;
-
         public NhanvienDTO(string maNV, string cCCD, string ten, string dia_chi, string email, string sdt, int maCV)
         {
             MaNV = maNV;
@@ -27,21 +23,9 @@ namespace NLBank.client.DTO
             Dia_chi = dia_chi;
             Email = email;
             Sdt = sdt;
+            MaCV = maCV;
         }
         public NhanvienDTO() { }
-        public NhanvienDTO(DataRow row) { 
-            MaNV = row["MaNV"].ToString();
-            CCCD = row["CCCD"].ToString();
-            Ten = row["Ten"].ToString();
-            Dia_chi = row["Dia_chi"].ToString();
-            Email = row["Dia_chi"].ToString();
-            Sdt = row["Dia_chi"].ToString();
-            MaCV = (int)row["MaCV"];
-            DataTable data = Connection.Instance.ExcuteQuery($"SELECT * FROM CHUCVU WHERE MaCV = {MaCV}");
-            foreach (DataRow item in data.Rows)
-            {
-                ChucVu = new ChucVuDTO(item);
-            }
-        }
+        
     }
 }
